05b567b39383d871919610b1c7ef349356c1c3e7
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-11 10:29:51 -0700

    Initialize project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bedrock
+
+React Component Library based on Bootstrap UI kit.
+
+## License
+
+Apache License v2.
---
+++ styleguide.config.js
@@ -0,0 +1,26 @@
+const glob = require( "glob" );
+const path = require( "path" );
+const fs = require( "fs" );
+
+module.exports = {
+    components: 'src/components/**/*.{ts,tsx}',
+    title : 'Bedrock Component Library',
+    pagePerSection: true,
+    propsParser: require( "react-docgen-typescript" ).parse,
+    webpackConfig: Object.assign( {}, require( "./webpack.config" ), {} ),
+    styleguideDir: "docs",
+    template : {
+        head : {
+            links : [
+                {
+                    rel : 'stylesheet',
+                    href : 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+                },
+                {
+                    rel : 'stylesheet',
+                    href : './assets/bedrock-docs.css'
+                }
+            ]
+        }
+    }
+};
