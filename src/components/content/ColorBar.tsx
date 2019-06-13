8c87769b495b33089b50496bbad835b0ccd7dd8e
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-13 16:47:46 -0700

    add ColorBar component

---
+++ src/components/content/ColorBar.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import HBox from './../layout/HBox';
+import ColorBox from './ColorBox';
+
+interface ColorBarProps {
+    /**
+     * An array of colors specified as a string, or an object
+     */
+    colors?: Array<any>;
+
+    /**
+     * Click handler to be invoked when any color is clicked on
+     */
+    onClick?: React.MouseEventHandler;
+}
+
+export default class ColorBar extends React.Component<ColorBarProps, any> {
+
+    getColors = () => {
+        let colors = this.props.colors;
+        let result = [];
+
+        for (let index = 0; index < colors.length; index++) {
+            const item = colors[index];
+
+            let color: string = '';
+            let label = '';
+            if (typeof item === 'string') {
+                color = item;
+            }
+            if (typeof item === 'object') {
+                color = item.color || '';
+                label = item.label || '';
+            }
+
+            result.push(<ColorBox color={color} label={label} />);
+        }
+
+        return result;
+    }
+
+    render() {
+        if (!this.props.colors) {
+            return null;
+        }
+
+        return <HBox>{this.getColors()}</HBox>;
+    }
+}
