6c5c7fc3104251997fb02daff22c04bc7edceff4
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-13 08:25:03 -0700

    add isOpen prop to alert

--- src/components/notify/Alert.tsx
+++ src/components/notify/Alert.tsx
@@ -3,12 +3,17 @@ import * as React from 'react';
 interface AlertProps {
     className?: string;
     variant?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
+    isOpen?: boolean;
 }
 
 export default class Alert extends React.Component<AlertProps, any> {
-    
+
     render() {
-        return <div className={ 'alert alert-' + this.props.variant } role="alert">
+        if (!this.props.isOpen) {
+            return null;
+        }
+        
+        return <div className={'alert alert-' + this.props.variant} role="alert">
             {this.props.children}
         </div>;
     }
