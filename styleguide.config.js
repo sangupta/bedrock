6fd4c1dc4c0aefbb9bf5cad58dec347ca05791c8
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-13 14:43:21 -0700

    sort component list

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,8 +2,25 @@ const glob = require( "glob" );
 const path = require( "path" );
 const fs = require( "fs" );
 
+function getComponents() {
+    let files = glob.sync('src/components/**/*.{ts,tsx}');
+
+    // sort based on file name and not path
+    files.sort(function(file1, file2) {
+        let index1 = file1.lastIndexOf('/');
+        let index2 = file2.lastIndexOf('/');
+        
+        let name1 = file1.substring(index1 + 1);
+        let name2 = file2.substring(index2 + 1);
+
+        return name1.localeCompare(name2);
+    });
+
+    return files;
+};
+
 module.exports = {
-    components: 'src/components/**/*.{ts,tsx}',
+    components: getComponents,
     title : 'Bedrock Component Library',
     pagePerSection: true,
     propsParser: require( "react-docgen-typescript" ).parse,
