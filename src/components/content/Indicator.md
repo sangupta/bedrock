ab0ee45579121bded03578efcc3810f2bb57bde1
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-14 00:29:14 -0700

    complete Indicator component

--- src/components/content/Indicator.md
+++ src/components/content/Indicator.md
@@ -1,5 +1,10 @@
-Useful to show the current status of an operation.
-
 ```js
 <Indicator variant='success' label='Upload complete.' />
+<Indicator variant='primary' label='Network connected.' />
+<Indicator variant='secondary' label='Not connected to server.' />
+<Indicator variant='danger' label='Upload failed.' />
+<Indicator variant='warning' label='Connection slow.' />
+<Indicator variant='info' label='No new messages.' />
+<Indicator variant='light' label='Dark mode indicator.' />
+<Indicator variant='dark' label='Feature unavailable.' />
 ```
--- src/components/content/Indicator.tsx
+++ src/components/content/Indicator.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
 
 interface IndicatorProps {
+    /**
+     * Text label to be displayed along side the indicator
+     */
     label?: string;
+
+    /**
+     * Custom CSS classes to be applied to the element
+     */
+    className?: string;
+
+    /**
+     * Element styling to be applied
+     */
     variant?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
 }
 
+/**
+ * Used to show the current status of an operation.
+ */
 export default class Indicator extends React.Component<IndicatorProps, any> {
 
     static defaultProps = {
@@ -13,7 +28,8 @@ export default class Indicator extends React.Component<IndicatorProps, any> {
     }
 
     render() {
-        return <div className={'indicator indicator-' + this.props.variant}>
+        return <div className='indicator'>
+            <span className={'indicator-dot bg-' + this.props.variant}></span>
             {this.props.label}
         </div>;
     }
