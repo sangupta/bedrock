737784e34f868ec5d884ae8abd7d26b3a7786c21
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-15 09:42:41 -0700

    add Navbar component boiler plate

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { BaseProps } from './../../BedrockUtils';
+
+interface NavbarProps extends BaseProps {
+
+}
+
+/**
+ * Useful to display a navigation bar.
+ */
+export default class Navbar extends React.Component<NavbarProps, any> {
+    render() {
+        return null;
+    }
+}
