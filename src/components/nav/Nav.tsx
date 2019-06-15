b92181c92d3bb2c770a74af869175f7475f15c97
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-15 09:42:22 -0700

    add Nav component boiler plate

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { BaseProps } from './../../BedrockUtils';
+
+interface NavProps extends BaseProps {
+
+}
+
+/**
+ * Useful to display basic navigation.
+ */
+export default class Nav extends React.Component<NavProps, any> {
+    render() {
+        return null;
+    }
+}
