51d80e450672b7b619dc20ede579fdf1fa1f1c94
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-15 00:30:27 -0700

    updated Toast component

--- src/components/notify/Toast.md
+++ src/components/notify/Toast.md
@@ -3,4 +3,7 @@ Push notifications to your visitors with a toast, a lightweight and easily custo
 Toasts are lightweight notifications designed to mimic the push notifications that have been popularized by mobile and desktop operating systems. They’re built with flexbox, so they’re easy to align and position.
 
 ```js
+<Toast title='Bedrock' subTitle='June 2019' closeable={true}>
+Hello, world! This is a toast message.
+</Toast>
 ```
--- src/components/notify/Toast.tsx
+++ src/components/notify/Toast.tsx
@@ -3,10 +3,73 @@ import { BaseProps } from './../../BedrockUtils';
 
 interface ToastProps extends BaseProps {
 
+    /**
+     * Image source for the icon image in toast header.
+     */
+    imageSrc?: string;
+
+    /**
+     * Alternate text to be shown on the icon image.
+     */
+    imageAlt?: string;
+
+    /**
+     * Title to be shown in toast header.
+     */
+    title?: string;
+
+    /**
+     * Sub-title to be shown in toast header.
+     */
+    subTitle?: string;
+
+    /**
+     * Whether to show the close button or not.
+     */
+    closeable?: boolean;
 }
 
 export default class Toast extends React.Component<ToastProps, any> {
+
+    static defaultProps = {
+        imageSrc: '',
+        imageAlt: '',
+        title: '',
+        subTitle: '',
+        closeable: true
+    }
+
+    getToastHeader = () => {
+        let header = [];
+
+        if (this.props.imageSrc) {
+            header.push(<img src={this.props.imageSrc} className="rounded mr-2" alt={this.props.imageAlt} />);
+        }
+        if (this.props.title) {
+            header.push(<strong className="mr-auto">{this.props.title}</strong>);
+        }
+        if (this.props.subTitle) {
+            header.push(<small>{this.props.subTitle}</small>);
+        }
+        if (this.props.closeable) {
+            header.push(<button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>);
+        }
+
+        if (header.length === 0) {
+            return null;
+        }
+
+        return <div className="toast-header">{header}</div>;
+    }
+
     render() {
-        return null;
+        return <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            {this.getToastHeader()}
+            <div className="toast-body">
+                {this.props.children}
+            </div>
+        </div>;
     }
 }
