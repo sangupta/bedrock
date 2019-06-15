3f8ca6eeeac899b8c3d025e1ac9d48f61508934e
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-15 12:35:12 -0700

    add docs to Well

--- src/components/containers/Well.tsx
+++ src/components/containers/Well.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { BaseProps } from './../../BedrockUtils';
 
+/**
+ * If you need a CSS styled `variant`-aware well to display
+ * content, consider using `Alert` component.
+ * 
+ */
 export default class Well extends React.Component<BaseProps, any> {
     render() {
         return <div className='card card-body'>
