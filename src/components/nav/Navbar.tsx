3941a961e0ff4c0cf19f9224c43547235a77e88b
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-19 15:44:08 -0700

    add Navbar component

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { BaseProps } from './../../BedrockUtils';
+import { BaseProps, mergeCSS } from './../../BedrockUtils';
 
 interface NavbarProps extends BaseProps {
+
+    theme?: 'light' | 'dark';
+
+    background?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
 
 }
 
@@ -9,7 +13,21 @@ interface NavbarProps extends BaseProps {
  * Useful to display a navigation bar.
  */
 export default class Navbar extends React.Component<NavbarProps, any> {
-    render() {
-        return null;
+
+    static defaultProps = {
+        theme: 'light',
+        background: 'light'
     }
+
+    render() {
+        const css: string = mergeCSS('navbar', {
+            'navbar-': this.props.theme,
+            'bg-': this.props.background
+        }, this.props.className);
+
+        return <nav className={css}>
+            {this.props.children}
+        </nav>;
+    }
+
 }
