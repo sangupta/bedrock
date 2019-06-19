3b7a0de18c708b93da935b65db8ba3a9ce742d15
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-19 13:52:10 -0700

    add ButtonGroup component

---
+++ src/components/collection/ButtonGroup.md
@@ -0,0 +1,35 @@
+```js
+import Button from './../button/Button';
+
+<ButtonGroup>
+    <Button>Left</Button>
+    <Button>Middle</Button>
+    <Button>Right</Button>
+</ButtonGroup>
+```
+
+## Sizing
+
+### Large
+
+```js
+import Button from './../button/Button';
+
+<ButtonGroup size='large'>
+    <Button>Left</Button>
+    <Button>Middle</Button>
+    <Button>Right</Button>
+</ButtonGroup>
+```
+
+### Small
+
+```js
+import Button from './../button/Button';
+
+<ButtonGroup size='small'>
+    <Button>Left</Button>
+    <Button>Middle</Button>
+    <Button>Right</Button>
+</ButtonGroup>
+```
---
+++ src/components/collection/ButtonGroup.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { BaseProps, mergeCSS } from './../../BedrockUtils';
+
+interface ButtonGroupProps extends BaseProps {
+    ariaLabel?: string;
+    role?: string;
+
+    /**
+     * Size of the button
+     */
+    size?: 'large' | 'small' | 'default';
+}
+
+export default class ButtonGroup extends React.Component<ButtonGroupProps, any> {
+
+    static defaultProps = {
+        role: 'group'
+    }
+
+    render() {
+        const css: string = mergeCSS('btn-group', {
+            'btn-group-lg': this.props.size === 'large',
+            'btn-group-sm': this.props.size === 'small'
+        }, this.props.className);
+
+        return <div className={css} role={this.props.role} aria-label={this.props.ariaLabel}>
+            {this.props.children}
+        </div>;
+    }
+}
