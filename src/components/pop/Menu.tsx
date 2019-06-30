ba1edb8d5a61b8c51fc725f7b63b18b7f1a1dc15
Sandeep Gupta <sandy.pec@gmail.com>
2019-06-30 01:50:27 -0700

    add outside tracking to menu

--- src/components/pop/Menu.tsx
+++ src/components/pop/Menu.tsx
@@ -10,7 +10,22 @@ interface MenuProps extends BaseProps {
      */
     show?: boolean;
 
+    /**
+     * Whether to use HTML `relative` or `absolute` positioning
+     * when rendering the menu
+     */
     position?: 'relative' | 'absolute';
+
+    /**
+     * Event handler called when a click happens outside
+     * the component.
+     */
+    onOutsideClick?: React.MouseEventHandler;
+
+    /**
+     * Whether to track click outside of the menu or not
+     */
+    trackOutsideClick?: boolean;
 }
 
 /**
@@ -19,9 +34,46 @@ interface MenuProps extends BaseProps {
  */
 export default class Menu extends React.Component<MenuProps, any> {
 
+    menuRef: any;
+
+    constructor(props) {
+        super(props);
+
+        this.menuRef = React.createRef();
+    }
+
     static defaultProps = {
         show: true,
-        position: 'relative'
+        position: 'relative',
+        trackOutsideClick: false
+    }
+
+    componentDidMount() {
+        if (this.props.trackOutsideClick) {
+            document.addEventListener('click', this.clickHandler);
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.props.trackOutsideClick) {
+            document.removeEventListener('click', this.clickHandler);
+        }
+    }
+
+    clickHandler = (e) => {
+        if (!this.props.trackOutsideClick) {
+            return;
+        }
+
+        const node = this.menuRef.current;
+        const isDescendantOfRoot = node && node.contains(e.target);
+        if (isDescendantOfRoot) {
+            return;
+        }
+
+        if (this.props.onOutsideClick) {
+            this.props.onOutsideClick(e);
+        }
     }
 
     render() {
@@ -30,7 +82,7 @@ export default class Menu extends React.Component<MenuProps, any> {
             'position-relative': this.props.position === 'relative'
         }, this.props.className);
 
-        return <div className={css} aria-labelledby="dropdownMenuButton">
+        return <div ref={this.menuRef} className={css} aria-labelledby="dropdownMenuButton">
             {this.props.children}
         </div>;
     }
