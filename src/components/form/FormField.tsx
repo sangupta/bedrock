b1802ae25340deda4ddca5826ff1e75518e7ec4d
Sandeep Gupta <sandy.pec@gmail.com>
2019-07-27 21:43:45 +0530

    add FormField component

---
+++ src/components/form/FormField.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { BaseProps, mergeCSS } from './../../BedrockUtils';
+
+interface FormFieldProps extends BaseProps {
+    label?: string;
+    for?: string;
+    help?: string;
+}
+
+export default class FormField extends React.Component<FormFieldProps, any> {
+
+    static defaultProps = {
+        for: '',
+        help: '',
+        label: ''
+    }
+
+    getLabel = () => {
+        if (!this.props.label) {
+            return null;
+        }
+
+        const extraProps = {
+            for:this.props.for
+        };
+        return <label {...extraProps}>{this.props.label}</label>;
+    }
+
+    getHelp = () => {
+        if(!this.props.help) {
+            return null;
+        }
+
+        return <small className="form-text text-muted">{this.props.help}</small>;
+    }
+
+    render() {
+        const css: string = mergeCSS('form-group', this.props.className);
+
+        return <div className={css}>
+            {this.getLabel()}
+            {this.props.children}
+            {this.getHelp()}
+        </div>;
+    }
+
+}
