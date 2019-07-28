21de3075d5c414d0a961470cd2901bf339e78770
Sandeep Gupta <sandy.pec@gmail.com>
2019-07-28 07:36:14 +0530

    export newer components

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
+// assets
 import FontAwesome from './components/assets/FontAwesome';
 import Gravatar from './components/assets/Gravatar';
 
+// buttons
 import BadgeButton from './components/button/BadgeButton';
 import Button from './components/button/Button';
 import SplitButton from './components/button/SplitButton';
 
+// collection
 import ButtonGroup from './components/collection/ButtonGroup';
 import ColorBar from './components/collection/ColorBar';
 import ListGroup from './components/collection/ListGroup';
 import ListGroupItem from './components/collection/ListGroupItem';
 
+// containers
 import Column from './components/containers/Column';
 import Jumbotron from './components/containers/Jumbotron';
 import Container from './components/containers/Container';
 
+// content
 import Badge from './components/content/Badge';
 import Code from './components/content/Code';
 import ColorBox from './components/content/ColorBox';
@@ -27,11 +32,20 @@ import Para from './components/content/Para';
 import Pre from './components/content/Pre';
 import Tag from './components/content/Tag';
 
-import Input from './components/form/Input';
+// form
+import Form from './components/form/Form';
+import FormButton from './components/form/FormButton';
+import FormField from './components/form/FormField';
+import PasswordInput from './components/form/PasswordInput';
+import Select from './components/form/Select';
+import TextArea from './components/form/TextArea';
+import TextInput from './components/form/TextInput';
 
+// layout
 import HBox from './components/layout/HBox';
 import VBox from './components/layout/VBox';
 
+// nav
 import Breadcrumb from './components/nav/Breadcrumb';
 import BreadcrumbItem from './components/nav/BreadcrumbItem';
 import Nav from './components/nav/Nav';
@@ -40,15 +54,25 @@ import NavItem from './components/nav/NavItem';
 import Pagination from './components/nav/Pagination';
 import PaginationItem from './components/nav/PaginationItem';
 
+// notify
 import Alert from './components/notify/Alert';
 import Toast from './components/notify/Toast';
 
+// pop
 import Dropdown from './components/pop/Dropdown';
 import Menu from './components/pop/Menu';
 import MenuItem from './components/pop/MenuItem';
 
+// progress
 import ProgressBar from './components/progress/ProgressBar';
 import Spinner from './components/progress/Spinner';
+
+// tables
+import Table from './components/tables/Table';
+import TableHead from './components/tables/TableHead';
+import TableBody from './components/tables/TableBody';
+import TableCol from './components/tables/TableCol';
+import TableRow from './components/tables/TableRow';
 
 export {
     // assets
@@ -88,7 +112,13 @@ export {
     Tag,
 
     // form
-    Input,
+    Form,
+    FormButton,
+    FormField,
+    PasswordInput,
+    Select,
+    TextArea,
+    TextInput,
 
     // layout
     HBox,
@@ -114,6 +144,16 @@ export {
 
     // progress
     ProgressBar,
-    Spinner
-    
+    Spinner,
+
+    // tables
+    Table,
+    TableHead,
+    TableBody,
+    TableCol,
+    TableRow,
+    TableRow as TR,
+    TableCol as TD,
+    TableHead as THead,
+    TableBody as TBody
 };
