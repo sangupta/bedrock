0716349cc81321af08a13a562c8c548987b0d048
Sandeep Gupta <sandy.pec@gmail.com>
2020-12-21 11:55:21 -0800

    Fix prop names for Table

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -5,28 +5,28 @@ interface TableProps extends BaseProps {
     /**
      * Whether to alternate color the rows
      */
-    isStriped?: boolean;
+    striped?: boolean;
 
     /**
      * Whether to use dark mode for the table
      */
-    isDark?: boolean;
+    darkMode?: boolean;
 
     /**
      * Whether the table has borders or not. 
      */
-    isBordered?: boolean;
+    bordered?: boolean;
 
     /**
      * Whether the table is borderless or not
      */
-    isBorderless?: boolean;
+    borderless?: boolean;
 
     /**
      * Whether the table is responsive across all break points. If set
      * will show horizontal scroll bar if the table does not fit in.
      */
-    isResponsive?: boolean;
+    responsive?: boolean;
 
     /**
      * Allow body rows to show hover effect when mouse rolls over them
@@ -42,21 +42,21 @@ interface TableProps extends BaseProps {
 export default class Table extends React.Component<TableProps, NoProps> {
 
     static defaultProps = {
-        isStriped: false,
-        isDark: false,
-        isBordered: false,
-        isBorderless: false,
+        striped: false,
+        darkMode: false,
+        bordered: false,
+        borderless: false,
         allowHover: false
     }
 
     render() {
         const css: string = mergeCSS('table', {
-            'table-striped': this.props.isStriped,
-            'table-dark': this.props.isDark,
-            'table-bordered': this.props.isBordered,
-            'table-borderless': this.props.isBorderless,
+            'table-striped': this.props.striped,
+            'table-dark': this.props.darkMode,
+            'table-bordered': this.props.bordered,
+            'table-borderless': this.props.borderless,
             'table-hover': this.props.allowHover,
-            'table-responsive': this.props.isResponsive
+            'table-responsive': this.props.responsive
         }, this.props.className);
 
         const extra:any = getProps(this.props);
