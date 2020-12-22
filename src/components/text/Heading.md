7b5a44aac45e9873f4d854908466b09f9f9be735
Sandeep Gupta <sandy.pec@gmail.com>
2020-12-21 19:35:29 -0800

    updated Heading component

--- src/components/text/Heading.md
+++ src/components/text/Heading.md
@@ -1,3 +1,5 @@
+### Default typography
+
 Useful to setup headings.
 
 ```js
@@ -7,4 +9,15 @@ Useful to setup headings.
 <Heading size={4}>h4. Bedrock heading</Heading>
 <Heading size={5}>h5. Bedrock heading</Heading>
 <Heading size={6}>h6. Bedrock heading</Heading>
+```
+
+### Display Mode
+
+```js
+<Heading displayMode={true} size={1}>h1. Bedrock heading</Heading>
+<Heading displayMode={true} size={2}>h2. Bedrock heading</Heading>
+<Heading displayMode={true} size={3}>h3. Bedrock heading</Heading>
+<Heading displayMode={true} size={4}>h4. Bedrock heading</Heading>
+<Heading displayMode={true} size={5}>h5. Bedrock heading</Heading>
+<Heading displayMode={true} size={6}>h6. Bedrock heading</Heading>
 ```
--- src/components/text/Heading.tsx
+++ src/components/text/Heading.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BaseProps, getProps, NoProps } from '../../BedrockUtils';
+import { BaseProps, getProps, NoProps, mergeCSS } from '../../BedrockUtils';
 
 interface HeadingProps extends BaseProps {
 
@@ -7,19 +7,27 @@ interface HeadingProps extends BaseProps {
      * The size of heading between 1 and 6.
      */
     size?: 1 | 2 | 3 | 4 | 5 | 6;
+
+    /**
+     * Enable display mode which renders larger opinionated heading style.
+     */
+    displayMode?: boolean;
 }
 
 export default class Heading extends React.Component<HeadingProps, NoProps> {
 
     static defaultProps = {
-        size: 1
+        size: 1,
+        displayMode: false
     }
 
     render() {
+        const dcss: string = this.props.displayMode ? 'display-' + this.props.size : '';
+        const css: string = mergeCSS(this.props.className, dcss);
         const Element: any = 'h' + this.props.size;
-        const extra:any = getProps(this.props);
-        
-        return <Element {...extra} className={this.props.className}>{this.props.children}</Element>
+        const extra: any = getProps(this.props);
+
+        return <Element {...extra} className={css}>{this.props.children}</Element>
     }
 
 }
