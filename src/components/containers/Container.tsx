8fa4d6f94ba5d60b4348a643e5374c394b4498f5
Sandeep Gupta <sandy.pec@gmail.com>
2020-12-21 16:35:52 -0800

    add breakpoint prop to Container

--- src/components/containers/Container.tsx
+++ src/components/containers/Container.tsx
@@ -2,22 +2,38 @@ import * as React from 'react';
 import { BaseProps, mergeCSS, getProps, NoProps } from './../../BedrockUtils';
 
 interface ContainerProps extends BaseProps {
+
     /**
      * Use full-width for the jumbotron without rounded corners.
      */
     fluid?: boolean;
+
+    /**
+     * Fluid breakpoints
+     */
+    breakpoint?: 'small' | 'medium' | 'large' | 'xl' | 'xxl'
+
 }
 
 export default class Container extends React.Component<ContainerProps, NoProps> {
 
     static defaultProps = {
-        fluid: false
+        fluid: false,
+        breakpoint: ''
     }
 
     render() {
-        const css: string = mergeCSS(this.props.fluid ? 'container-fluid' : 'container', this.props.className);
-        const extra:any = getProps(this.props);
-        
+        const bp: string = this.props.breakpoint;
+
+        const css: string = mergeCSS(this.props.fluid ? 'container-fluid' : 'container', {
+            'container-sm': bp === 'small',
+            'container-md': bp === 'medium',
+            'container-lg': bp === 'large',
+            'container-xl': bp === 'xl',
+            'container-xxl': bp === 'xxl',
+        }, this.props.className);
+        const extra: any = getProps(this.props);
+
         return <div {...extra} className={css}>
             {this.props.children}
         </div>;
