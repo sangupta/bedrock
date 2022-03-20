ee84984da06841d3f4e7ba14f92e0b9d1f014ee9
Sandeep Gupta <sandy.pec@gmail.com>
2022-03-19 21:43:06 -0700

    update Para and Heading components

--- src/components/typography/Para.tsx
+++ src/components/typography/Para.tsx
@@ -1,11 +1,71 @@
+/**
+ *
+ * bedrock - UI Component Library
+ * Copyright (c) 2020, Sandeep Gupta
+ *
+ * https://bedrock.sangupta.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
 import React from 'react';
 import { buildCss } from '../../Utils';
 
-export default class Para extends React.PureComponent<BaseProps> {
+/**
+ * Props for the component.
+ */
+interface ParaProps extends BaseProps {
+    variant?: Variant;
+
+    leadPara?: boolean;
+
+    textAlign?: Alignment;
+
+    wrapMode?: '' | 'wrap' | 'no-wrap';
+
+    transform?: TextTransform;
+}
+
+/**
+ * The paragraph component.
+ * 
+ * @author sangupta
+ */
+export default class Para extends React.PureComponent<ParaProps> {
+
+    /**
+     * the default properties
+     */
+    static defaultProps: ParaProps = {
+        variant: 'primary',
+        leadPara: false,
+        wrapMode: ''
+    }
 
     render(): React.ReactNode {
-        const { className, children, ...extraProps } = this.props;
-        const css = buildCss(className);
+        const { className, children, variant, leadPara, textAlign, wrapMode, transform, ...extraProps } = this.props;
+        const css = buildCss(className, {
+            'text-': variant,
+            'lead': leadPara,
+        }, {
+            'text-': textAlign,
+        }, {
+            'text-': wrapMode,
+        }, {
+            'text-': transform
+        });
 
         return <p className={css} {...extraProps}>{children}</p>
     }
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,4 +1,26 @@
 /**
+ *
+ * bedrock - UI Component Library
+ * Copyright (c) 2020, Sandeep Gupta
+ *
+ * https://bedrock.sangupta.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+/**
  * The usual `props` applied across all components.
  * 
  */
@@ -11,6 +33,17 @@ interface BaseProps {
 }
 
 /**
- * Usual variants that apply mostly across Bootstrap
+ * Usual variants that apply mostly across Bootstrap.
  */
 type Variant = 'primary' | 'secondary' | 'info' | 'success' | 'danger' | 'warning' | 'light' | 'dark';
+
+/**
+ * Ways in which something can be aligned respective to
+ * the container.
+ */
+type Alignment = 'start' | 'center' | 'end';
+
+/**
+ * Applicable text transformations
+ */
+type TextTransform = 'lowercase' | 'uppercase' | 'capitalize';
