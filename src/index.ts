9b50eb2873daf86ebe385cdb666705bc00a7179d
Sandeep Gupta <sandy.pec@gmail.com>
2022-03-22 21:09:32 -0700

    update index for exports

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,56 @@
  *
  */
 
+// content imports
+import ColorBox from "./components/content/ColorBox";
+import Indicator from "./components/content/Indicator";
+import StaticContent from "./components/content/StaticContent";
+
+// feedback imports
+import Alert from "./components/feedback/Alert";
+
+// form imports
+import Button from "./components/form/Button";
+import ButtonGroup from "./components/form/ButtonGroup";
+import Form from "./components/form/Form";
+
+// layout imports 
+import FlexBox from "./components/layout/FlexBox";
+import HBox from "./components/layout/HBox";
+import VBox from "./components/layout/VBox";
+
+// media imports
+import Image from "./components/media/Image";
+
+// typography imports
 import Code from "./components/typography/Code";
 import Heading from "./components/typography/Heading";
 import Para from "./components/typography/Para";
 
 // Export all the components from this library
 export {
-    // typography components
+    // content
+    ColorBox,
+    Indicator,
+    StaticContent,
+
+    // feedback
+    Alert,
+
+    // form
+    Button,
+    ButtonGroup,
+    Form,
+
+    // layout
+    FlexBox,
+    HBox,
+    VBox,
+
+    // media
+    Image,
+
+    // typography
     Code,
     Heading,
     Para
