33ddb3bb5162683a0bd6d6d8c88431007e0addc1
Sandeep Gupta <sandy.pec@gmail.com>
2022-03-31 16:54:16 -0700

    add Countdown component

---
+++ src/components/content/Countdown.tsx
@@ -0,0 +1,109 @@
+/**
+ *
+ * bedrock - UI Component Library
+ * Copyright (c) 2020, Sandeep Gupta
+ *
+ * https://bedrock.sangupta.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import React from 'react';
+
+/**
+ * Props for the component
+ */
+interface CountdownProps {
+    /**
+     * The starting value
+     */
+    start: number;
+
+    /**
+     * The ending value
+     */
+    stop: number;
+
+    /**
+     * The stepping value to increment/decrement with
+     */
+    step?: number
+
+    /**
+     * Delay between each update
+     */
+    timeDelay?: number;
+}
+
+interface CountdownState {
+    value: number;
+}
+
+export default class Countdown extends React.PureComponent<CountdownProps, CountdownState> {
+
+    static defaultProps = {
+        timeDelay: 1000,
+        step: 1
+    }
+
+    /**
+     * Reference to the timer that we create
+     */
+    timer: number;
+
+    timerCreated: boolean = false;
+
+    constructor(props: CountdownProps) {
+        super(props);
+
+        this.state = {
+            value: props.start
+        }
+    }
+
+    componentDidMount(): void {
+        this.timer = setInterval(this.updateValue, this.props.timeDelay);
+        this.timerCreated = true;
+    }
+
+    componentWillUnmount(): void {
+        if (this.timerCreated) {
+            clearInterval(this.timer);
+        }
+    }
+
+    updateValue = (): void => {
+        const { step, stop } = this.props;
+        let { value } = this.state;
+        value += step;
+        if (step > 0 && value <= stop) {
+            this.setState({ value: value });
+            return;
+        }
+
+        if (step < 0 && value >= stop) {
+            this.setState({ value: value });
+            return;
+        }
+
+        // we are done updating - stop
+        clearInterval(this.timer);
+        this.timerCreated = false;
+    }
+
+    render(): React.ReactNode {
+        return this.state.value
+    }
+
+}
