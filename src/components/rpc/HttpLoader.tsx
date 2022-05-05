55a95b38d6048907c44c8051a87bbc6577d4e376
Sandeep Gupta <sandy.pec@gmail.com>
2022-05-05 14:20:26 -0700

    add HttpLoader component

---
+++ src/components/rpc/HttpLoader.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+interface HttpLoaderProps {
+    httpCallInvoker: () => Promise<any>;
+
+    /**
+     * Rendered while data is loading
+     */
+    whenLoad: React.ReactNode | (() => React.ReactNode);
+
+    /**
+     * Rendered when we have an error loading data
+     */
+    whenError: React.ReactNode | ((e: any) => React.ReactNode);
+
+    /**
+     * Called when we have an error fetching data
+     */
+    onError?: (e: any) => void;
+
+    /**
+     * Rendered when we have success receiving data
+     */
+    whenSuccess: (data: any) => React.ReactNode;
+
+    /**
+     * Called when we have a success and receive data
+     */
+    onDataSuccess?: (data: any) => void;
+}
+
+interface HttpLoaderState {
+    loading: boolean;
+    error: boolean;
+    errorData?: any;
+    data?: any;
+}
+
+export default class HttpLoader extends React.Component<HttpLoaderProps, HttpLoaderState> {
+
+    constructor(props: HttpLoaderProps) {
+        super(props);
+
+        this.state = {
+            loading: true,
+            error: false,
+            data: null
+        }
+    }
+
+    componentDidMount = async () => {
+        try {
+            const result = await this.props.httpCallInvoker();
+            this.setState({ loading: false, error: false, errorData: undefined, data: result });
+            if (this.props.onDataSuccess) {
+                this.props.onDataSuccess(result);
+            }
+        } catch (e) {
+            this.setState({ loading: false, error: true, errorData: e, data: null });
+            if (this.props.onError) {
+                this.props.onError(e);
+            }
+        }
+    }
+
+    render() {
+        const { loading, error, errorData, data } = this.state;
+
+        if (loading) {
+            if (typeof this.props.whenLoad === 'function') {
+                return this.props.whenLoad();
+            }
+
+            return this.props.whenLoad;
+        }
+
+        if (error) {
+            if (typeof this.props.whenError === 'function') {
+                return this.props.whenError(errorData);
+            }
+
+            return this.props.whenError;
+        }
+
+        return this.props.whenSuccess(data);
+    }
+
+}
