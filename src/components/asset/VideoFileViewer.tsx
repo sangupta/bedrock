a5a201e902cf6ed25e26d5873f27ad035c342307
Sandeep Gupta <sandy.pec@gmail.com>
2022-08-17 20:03:35 -0700

    add VideoFileViewer stub

---
+++ src/components/asset/VideoFileViewer.tsx
@@ -0,0 +1,40 @@
+/**
+ *
+ * bedrock - UI Component Library
+ * Copyright (c) 2020, Sandeep Gupta
+ *
+ * https://bedrock.sangupta.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+ import React from 'react';
+
+ interface VideoFileViewerProps {
+     url: string;
+ }
+ 
+ /**
+ * A basic image file viewer. It supports basic non-destructive 
+ * controls.
+ * 
+ */
+ export default class VideoFileViewer extends React.Component<VideoFileViewerProps> {
+ 
+     render() {
+         return null;
+     }
+ 
+ }
+ 
