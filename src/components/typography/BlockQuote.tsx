c112a1fd6f30c2d135f22d91c6a83a8117735763
Sandeep Gupta <sandy.pec@gmail.com>
2023-02-28 22:30:07 -0800

    add blockquote component

---
+++ src/components/typography/BlockQuote.tsx
@@ -0,0 +1,31 @@
+/**
+ * bedrock - UI Component Library
+ * https://github.com/sangupta/bedrock
+ *
+ * MIT License.
+ * Copyright (c) 2022, Sandeep Gupta.
+ *
+ * Use of this source code is governed by a MIT style license
+ * that can be found in LICENSE file in the code repository.
+ */
+
+import React from 'react';
+import { BaseProps } from '../../types';
+import { buildCss } from '../../Utils';
+
+/**
+ * The `blockquote` component.
+ * 
+ * @author sangupta
+ */
+export default class BlockQuote extends React.PureComponent<BaseProps> {
+
+    render(): React.ReactNode {
+        const { className, children, ...extraProps } = this.props;
+        const css: string = buildCss('blockquote', className);
+        return <blockquote {...extraProps} className={css}>
+            {children}
+        </blockquote>
+    }
+
+}
