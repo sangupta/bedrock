b73dfd9903c24618b352b70ba1f70b71588c2041
Sandeep Gupta <sandy.pec@gmail.com>
2023-03-07 15:13:08 -0800

    upgrade react and make tests work

--- jest.config.js
+++ jest.config.js
@@ -76,16 +76,16 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    "js",
+    // "mjs",
+    // "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    // "json",
+    // "node"
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -121,7 +121,7 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: "./test",
+  rootDir: "./",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -176,7 +176,7 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
--- test/components/content/Indicator.test.tsx
+++ test/components/content/Indicator.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { describe, expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import { Indicator } from './../../../src'
