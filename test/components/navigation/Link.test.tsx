16c8257609d7bf07252733d9eb82bf9eb581f249
Sandeep Gupta <sandy.pec@gmail.com>
2023-03-07 19:08:09 -0800

    add tests for Link

---
+++ test/components/navigation/Link.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Link } from '../../../src';
+
+describe('Link component', () => {
+
+    it('renders a element with correct text content and href', () => {
+        const text = 'This is a link';
+        const href = 'https://example.com';
+        const { getByText } = render(<Link href={href}>{text}</Link>);
+        const linkElement = getByText(text);
+        expect(linkElement.tagName).toBe('A');
+        expect(linkElement).toHaveAttribute('href', href);
+    });
+
+    it('renders a element with custom class name', () => {
+        const text = 'This is a link';
+        const href = 'https://example.com';
+        const className = 'test-class';
+        const { getByText } = render(
+            <Link href={href} className={className}>
+                {text}
+            </Link>
+        );
+        const linkElement = getByText(text);
+        expect(linkElement.tagName).toBe('A');
+        expect(linkElement).toHaveAttribute('href', href);
+        expect(linkElement).toHaveClass(className);
+    });
+
+    it('renders a element with target attribute', () => {
+        const text = 'This is a link';
+        const href = 'https://example.com';
+        const target = '_blank';
+        const { getByText } = render(
+            <Link href={href} target={target}>
+                {text}
+            </Link>
+        );
+        const linkElement = getByText(text);
+        expect(linkElement.tagName).toBe('A');
+        expect(linkElement).toHaveAttribute('href', href);
+        expect(linkElement).toHaveAttribute('target', target);
+    });
+    
+});
